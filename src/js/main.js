e7600e277d086b043dbec98ee1ceeed13d34f539
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2024-04-03 12:12:19 +1300

    Added and styled posts coming from /GET posts

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,50 @@
 console.log("Hello FROM HOMEPAGE")
-const blogList = document.body.querySelector(".content");
+import '../styles/homepage.css'
+const content = document.body.querySelector(".content");
 
 async function getposts() {
-    
+
     try {
         const response = await fetch('http://localhost:3000/api/posts/')
-        if(!response.ok){
+        if (!response.ok) {
             throw Error(response.status)
         }
         const data = await response.json()
+        const blogList = document.createElement('div')
+        blogList.classList.add('blog-list')
         data.forEach(blog => {
-            const testpara = document.createElement('li')
-            testpara.textContent = blog.title
-            blogList.appendChild(testpara)
-        });
+            console.log(blog)
+            const blogItem = document.createElement('div')
+            blogItem.classList.add('blog')
+            const blogLink = document.createElement('a')
+            blogLink.classList.add('blog-link')
+            blogLink.href = `/blog?id=${blog._id}`;
+            const blogInfo = document.createElement('div')
+            blogInfo.classList.add('blog-info')
 
+            const blogTitle = document.createElement('h2')
+            blogTitle.textContent = blog.title;
+
+            const blogAuthor = document.createElement('p')
+            blogAuthor.textContent = blog.author.username;
+
+            const blogText = document.createElement('div')
+            blogText.classList.add('blog-text')
+
+            const blogContent = document.createElement('p')
+            blogContent.textContent = blog.content;
+
+            blogText.appendChild(blogContent)
+            blogInfo.appendChild(blogTitle);
+            blogInfo.appendChild(blogAuthor);
+            blogLink.appendChild(blogInfo)
+            blogLink.appendChild(blogText)
+            blogItem.appendChild(blogLink)
+            blogList.appendChild(blogItem)
+
+        });
+        console.log(blogList)
+        content.appendChild(blogList)
     }
     catch (err) {
         console.log("error: " + err)
